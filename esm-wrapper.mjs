30b31b430d682af85e7a3e0a1b4bbda93e285938
Myles Borins <myles.borins@gmail.com>
2020-05-08 01:55:11 -0400

    fixup: review

--- esm-wrapper.mjs
+++ esm-wrapper.mjs
@@ -1,56 +1,6 @@
-// just pre-load all the stuff that index.js lazily exports
-import {createRequire} from 'module';
+import semver from './index.js'
 
-const require = createRequire(import.meta.url);
-
-const internalRe = require('./internal/re');
-
-const re = internalRe.re;
-const src = internalRe.src;
-const tokens = internalRe.t;
-const SEMVER_SPEC_VERSION = require('./internal/constants').SEMVER_SPEC_VERSION;
-const SemVer = require('./classes/semver');
-const compareIdentifiers = require('./internal/identifiers').compareIdentifiers;
-const rcompareIdentifiers = require('./internal/identifiers').rcompareIdentifiers;
-const parse = require('./functions/parse');
-const valid = require('./functions/valid');
-const clean = require('./functions/clean');
-const inc = require('./functions/inc');
-const diff = require('./functions/diff');
-const major = require('./functions/major');
-const minor = require('./functions/minor');
-const patch = require('./functions/patch');
-const prerelease = require('./functions/prerelease');
-const compare = require('./functions/compare');
-const rcompare = require('./functions/rcompare');
-const compareLoose = require('./functions/compare-loose');
-const compareBuild = require('./functions/compare-build');
-const sort = require('./functions/sort');
-const rsort = require('./functions/rsort');
-const gt = require('./functions/gt');
-const lt = require('./functions/lt');
-const eq = require('./functions/eq');
-const neq = require('./functions/neq');
-const gte = require('./functions/gte');
-const lte = require('./functions/lte');
-const cmp = require('./functions/cmp');
-const coerce = require('./functions/coerce');
-const Comparator = require('./classes/comparator');
-const Range = require('./classes/range');
-const satisfies = require('./functions/satisfies');
-const toComparators = require('./ranges/to-comparators');
-const maxSatisfying = require('./ranges/max-satisfying');
-const minSatisfying = require('./ranges/min-satisfying');
-const minVersion = require('./ranges/min-version');
-const validRange = require('./ranges/valid');
-const outside = require('./ranges/outside');
-const gtr = require('./ranges/gtr');
-const ltr = require('./ranges/ltr');
-const intersects = require('./ranges/intersects');
-const simplifyRange = require('./ranges/simplify');
-const subset = require('./ranges/subset');
-
-export default {
+export const {
   re,
   src,
   tokens,
@@ -95,51 +45,6 @@ export default {
   intersects,
   simplifyRange,
   subset
-};
+} = semver;
 
-export {
-  re,
-  src,
-  tokens,
-  SEMVER_SPEC_VERSION,
-  SemVer,
-  compareIdentifiers,
-  rcompareIdentifiers,
-  parse,
-  valid,
-  clean,
-  inc,
-  diff,
-  major,
-  minor,
-  patch,
-  prerelease,
-  compare,
-  rcompare,
-  compareLoose,
-  compareBuild,
-  sort,
-  rsort,
-  gt,
-  lt,
-  eq,
-  neq,
-  gte,
-  lte,
-  cmp,
-  coerce,
-  Comparator,
-  Range,
-  satisfies,
-  toComparators,
-  maxSatisfying,
-  minSatisfying,
-  minVersion,
-  validRange,
-  outside,
-  gtr,
-  ltr,
-  intersects,
-  simplifyRange,
-  subset
-};
+export default semver
